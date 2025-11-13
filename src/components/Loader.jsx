a0444175bc9255ca9d64fabb1126b7b3a0a77353
Rajia Bashir Rahi <rajiabashirrahi@Rajias-MacBook-Air.local>
2025-11-13 08:59:47 -0500

    add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="w-full flex items-center justify-center py-12">
+      <div className="animate-spin w-10 h-10 rounded-full border-4 border-slate-300 border-t-primary"></div>
+    </div>
+  );
+}
